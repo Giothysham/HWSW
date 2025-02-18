e84354b89dab2d339d0a16aa8931940408132f10
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-02-18 16:41:42 +0100

    sf
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- Oef_105_New/ch1/firmware/assignment/src/print.c
+++ Oef_105_New/ch1/firmware/assignment/src/print.c
@@ -54,7 +54,10 @@ void print_dec(unsigned int val) {
         buffer[i - 1 - j] = temp;
     }
 
-    print_str(&buffer[0]);
+    for (j = 0; buffer[j] != '\0'; j++) {
+        *((volatile unsigned int*)OUTPORT) = buffer[j];
+    }
+
 		
 }
 
