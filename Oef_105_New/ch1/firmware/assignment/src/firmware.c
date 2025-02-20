a3802cd83469c11ca6cd9c8e437c3a54679a623f
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-02-20 10:51:41 +0100

    testing
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- Oef_105_New/ch1/firmware/assignment/src/firmware.c
+++ Oef_105_New/ch1/firmware/assignment/src/firmware.c
@@ -1,13 +1,34 @@
 #include "print.h"
 
+char buffer_string[4] = {'1', '5', '6', '5'};
+char buffer_number[4] = "8855";
+
 int main(void) {
-	print_str("Hello, World!\n");
-	print_dec(9);
+	// print_str("Hello, World!\n");
+	// print_dec(9);
+	// print_str("\n");
+	// print_dec(32);
+	// print_str("\n");
+	// print_dec(320);
+	// print_str("\n");
+	// print_dec(7458);
+	// print_str("\n");
+
+	print_str("buffer_string with print_str\n");
+	print_str(&buffer_string);
 	print_str("\n");
-	print_dec(32);
+	print_str("buffer_number with print_str\n");
+	print_dec(&buffer_number);
 	print_str("\n");
-	print_dec(320);
+	print_str("buffer_string with print_chr\n");
+	for(int i = 0; i < 4; i++) {
+		print_chr(&buffer_string[i]);
+	}
 	print_str("\n");
-	print_dec(7458);
+	print_str("buffer_number with print_chr\n");
+	for(int i = 0; i < 4; i++) {
+		print_chr(&buffer_number[i]);
+	}
 	print_str("\n");
+
 }
