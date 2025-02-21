95a6cfe615179974e3b02e95b59bc923e279a35b
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-02-21 16:17:56 +0100

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- Oef_105_New/ch1/firmware/assignment/src/print.h
+++ Oef_105_New/ch1/firmware/assignment/src/print.h
@@ -16,5 +16,7 @@ void print_chr(char ch);
 void print_str(const char *p);
 void print_dec(unsigned int val);
 void print_hex(unsigned int val, int digits);
+int Pow(int x, int n);
+int mul(int x, int y);
 
 #endif
