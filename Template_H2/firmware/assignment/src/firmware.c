9ef6253edfeb9bbbbdca31a1efa48a356723eeaa
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-03-06 12:51:03 +0100

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- Template_H2/firmware/assignment/src/firmware.c
+++ Template_H2/firmware/assignment/src/firmware.c
@@ -1,13 +1,10 @@
-
 #define OUTPORT 0x80000000
 
+
 int main(void) {
-    int i = 0;
-    if (i == 0) {
-        *((volatile unsigned int*)OUTPORT) = 3;
-        i = 1;
-    } else {
-        *((volatile unsigned int*)OUTPORT) = 0;
-        i = 0;
-    }
+	int i;
+	for (i = 0; i < 10; i++) {
+		*((volatile unsigned int *)OUTPORT) = i;
+	}
+	return 0;
 }
