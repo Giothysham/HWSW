7423c6dbab0da69da5553988cd05fd58a034c0e0
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-03-06 14:02:00 +0100

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- Template_H2/firmware/assignment/src/firmware.c
+++ Template_H2/firmware/assignment/src/firmware.c
@@ -6,10 +6,10 @@ int main(void) {
 	for (;;) {
 		for(int j = 0 ; j <= 4000000; j++){
 			if(j == 4000000){
-				if(i == 7 && j == 4000000){
+				if(i == 7){
 					i = 0;
 					*((volatile unsigned int *)OUTPORT) = i;
-				} else if(i == 0 && j == 4000000) {
+				} else if(i == 0) {
 					i = 7;
 					*((volatile unsigned int *)OUTPORT) = i;
 				}
