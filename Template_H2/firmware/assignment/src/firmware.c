5a9db50616d0ac64021f533bfc75c19a1980f232
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-03-12 17:47:07 +0100

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- Template_H2/firmware/assignment/src/firmware.c
+++ Template_H2/firmware/assignment/src/firmware.c
@@ -1,10 +1,12 @@
-#define OUTPORT 0x80000000
+#include "TCNT.h"
 
+#define OUTPORT 0x80000000
 
 int main(void) {
 	int i = 0;
 	for (;;) {
 		for(int j = 0 ; j <= 40; j++){
+			TCNT_start();
 			if(j == 40){
 				if(i <= 15){
 					i = i + 1;
@@ -14,6 +16,8 @@ int main(void) {
 					*((volatile unsigned int *)OUTPORT) = i;
 				}
 			}
+			TCNT_stop();
+			*((volatile unsigned int *)OUTPORT) = TCNT_VAL;
 		}
 	}
 	return 0;
