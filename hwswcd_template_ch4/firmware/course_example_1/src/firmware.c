826f9fab1c0caf2ff2e653e91dc3ce5ae64bc88d
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-03-20 16:05:40 +0100

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- hwswcd_template_ch4/firmware/course_example_1/src/firmware.c
+++ hwswcd_template_ch4/firmware/course_example_1/src/firmware.c
@@ -11,12 +11,12 @@ unsigned int i=1, j;
 
 void irq_handler(unsigned int cause) {
 
-    if (0<=i<8) {
+    if (i < 4) {
         i = i + 1;
-        LED = i;
+        LED = 1 << i;
     } else {
         i = 0;
-        LED = i;
+        LED = 0;
     }
 
     TCNT_CR = 0x17;
@@ -29,11 +29,16 @@ void main(void) {
     
     
 
-    TCNT_CMP = 0xC65D40;
+    TCNT_CMP = 0xff;
     TCNT_start();
     
 
     while(1) {
+        for(int j=0; j<0xC65D40; j++);
+        if(i == ) {
+            LED = i | 0b1000;
+        }
+        
         
     }
 }
