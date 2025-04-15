17fda92d057131e2a376c1170ca98e7653763e2c
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-04-15 16:02:16 +0200

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- hwswcd_template_ch4/firmware/course_example_1/src/firmware.c
+++ hwswcd_template_ch4/firmware/course_example_1/src/firmware.c
@@ -158,10 +158,10 @@ int main(void) {
         position++;
     }
 
-    save_compression((unsigned long long int*)&header.width, 4);
-    save_compression((unsigned long long int*)&header.height, 4);
-    save_compression((unsigned long long int*)&header.channels, 1);
-    save_compression((unsigned long long int*)&header.colorspace, 1);
+    save_compression(header.width, 4);
+    save_compression(header.height, 4);
+    save_compression(header.channels, 1);
+    save_compression(header.colorspace, 1);
 
     /* Loop over pixels */
     for(unsigned char h=0;h<C_HEIGHT;h++) {
