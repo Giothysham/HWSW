98f36887738177da4ede7d6b516291b9fc40976d
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-04-22 14:10:25 +0200

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- hwswcd_template_ch4/firmware/course_example_1/src/firmware.c
+++ hwswcd_template_ch4/firmware/course_example_1/src/firmware.c
@@ -227,9 +227,9 @@ int main(void) {
     }
 
     LED = 0x00;
-    LED = 0x01;
-    LED = 0x02;
-    LED = 0x03;
+    LED = 0x00;
+    LED = 0x00;
+    LED = 0x00;
     LED = 0x04;
     LED = 0x05;
     LED = 0x06;
