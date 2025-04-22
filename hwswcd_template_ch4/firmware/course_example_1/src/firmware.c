107d89afc24d34739ca35fcef3103dac79266a4e
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-04-22 16:28:00 +0200

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- hwswcd_template_ch4/firmware/course_example_1/src/firmware.c
+++ hwswcd_template_ch4/firmware/course_example_1/src/firmware.c
@@ -151,8 +151,8 @@ int main(void) {
     save_compression(header.width, 4);
     save_compression(header.height, 4);
 
-    LED = header.channels;
     LED = header.colorspace;
+    LED = header.channels;
     // save_compression(header.channels, 1);
     // save_compression(header.colorspace, 1);
 
