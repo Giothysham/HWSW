1d0c1e6438aac021a451491e247982497c238832
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-04-23 14:04:12 +0200

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- hwswcd_template_ch4/firmware/course_example_1/src/firmware.c
+++ hwswcd_template_ch4/firmware/course_example_1/src/firmware.c
@@ -1,33 +1,225 @@
-//#include <stdio.h>
-
-#include "tcnt.h"
+// #include <stdio.h>
 #include "sensor.h"
+#include "tcnt.h"
 
-#define LED_BASEAxDDRESS 0x80000000
+#define OUT_BASE_ADDRESS 0x80000000
+#define OUT_REG0_ADDRESS (OUT_BASE_ADDRESS + 0*4)
+#define OUTPUT           (*(volatile unsigned int *) OUT_REG0_ADDRESS)
 
-#define LED_REG0_ADDRESS (LED_BASEAxDDRESS + 0*4)
+extern unsigned int sw_mult(unsigned int x, unsigned int y);// Replace with your actual implementation
 
-#define LED (*(volatile unsigned int *) LED_REG0_ADDRESS)
+void irq_handler(unsigned int cause) {}
 
-void irq_handler(unsigned int cause) {
+// void initialise(unsigned char r[C_WIDTH][C_HEIGHT], unsigned char g[C_WIDTH][C_HEIGHT], unsigned char b[C_WIDTH][C_HEIGHT], unsigned char a[C_WIDTH][C_HEIGHT]) {
+//     unsigned char w, h;
+//     for(h=0; h<4; h++) {
+//         for(w=0; w<4; w++) {
+//             r[h][w] = 255; g[h][w] = 0; b[h][w] = 0; a[h][w] = 255;
+//         }
+//         for(w=4; w<C_WIDTH; w++) {
+//             r[h][w] = 0; g[h][w] = 255; b[h][w] = 0; a[h][w] = 255;
+//         }
+//     }
+//     for(h=4; h<C_HEIGHT; h++) {
+//         for(w=0; w<4; w++) {
+//             r[h][w] = 0; g[h][w] = 0; b[h][w] = 255; a[h][w] = 255;
+//         }
+//         for(w=4; w<C_WIDTH; w++) {
+//             r[h][w] = 127; g[h][w] = 127; b[h][w] = 127; a[h][w] = 255;
+//         }
+//     }
+// }
 
-        LED = 0x00FF00FF;
-
-}
+// void initialise(unsigned char r[C_HEIGHT][C_WIDTH], unsigned char g[C_HEIGHT][C_WIDTH], unsigned char b[C_HEIGHT][C_WIDTH], unsigned char a[C_HEIGHT][C_WIDTH]) {
+//     for (unsigned char h = 0; h < C_HEIGHT; h++) {
+//         for (unsigned char w = 0; w < C_WIDTH; w++) {
+//             unsigned int pixel = SENSOR_fetch();
+//             r[h][w] = (pixel >> 24) & 0xFF;
+//             g[h][w] = (pixel >> 16) & 0xFF;
+//             b[h][w] = (pixel >> 8)  & 0xFF;
+//             a[h][w] = (pixel >> 0)  & 0xFF;
+//         }
+//     }
+// }
 
 int main(void) {
 
-    unsigned char number;
+    unsigned char C_WIDTH = SENSOR_get_width();
+    unsigned char C_HEIGHT = SENSOR_get_height();
 
-    number = SENSOR_get_width();
+    unsigned char r[C_HEIGHT][C_WIDTH];
+    unsigned char g[C_HEIGHT][C_WIDTH];
+    unsigned char b[C_HEIGHT][C_WIDTH];
+    unsigned char a[C_HEIGHT][C_WIDTH];
+    
+    //initialise(r, g, b, a);
+    for (unsigned char h = 0; h < C_HEIGHT; h++) {
+        for (unsigned char w = 0; w < C_WIDTH; w++) {
+            unsigned int pixel = SENSOR_fetch();
+            r[h][w] = (pixel >> 24) & 0xFF;
+            g[h][w] = (pixel >> 16) & 0xFF;
+            b[h][w] = (pixel >> 8)  & 0xFF;
+            a[h][w] = (pixel >> 0)  & 0xFF;
+        }
+    }
+    
+    unsigned char r_prev = 0;
+    unsigned char g_prev = 0;
+    unsigned char b_prev = 0;
+    unsigned char a_prev = 255;
+    
+    signed char dr;
+    signed char dg;
+    signed char db;
+    
+    signed char rle = -1;
+    unsigned int running_array[64];
 
-    LED = number;
 
-    number = SENSOR_get_height();
+    for(unsigned char i = 0; i < 64; i++) {
+        running_array[i] = 0;
+    }
 
-    LED = number;
+    /* Header */
+    OUTPUT = (unsigned char)'q';
+    OUTPUT = (unsigned char)'o';
+    OUTPUT = (unsigned char)'i';
+    OUTPUT = (unsigned char)'f';
+
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = C_WIDTH;
+
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = C_HEIGHT;
+
+    OUTPUT = 0x03;
+    OUTPUT = 0x00;
+
+    for(unsigned char h = 0; h < C_HEIGHT; h++) {
+        for(unsigned char w = 0; w < C_WIDTH; w++) {
+            unsigned char cr = r[h][w];
+            unsigned char cg = g[h][w];
+            unsigned char cb = b[h][w];
+            unsigned char ca = a[h][w];
+
+            if (cr == r_prev && cg == g_prev && cb == b_prev && ca == a_prev) {
+                if (rle == -1) rle = 1;
+                else rle++;
+
+                if (rle == 62) {
+                    OUTPUT = (0b11000000 | (rle - 1));
+                    rle = -1;
+                }
+                continue;
+            }
+
+            if (rle > 0) {
+                OUTPUT = (0b11000000 | (rle - 1));
+                rle = -1;
+            }
+
+            unsigned char index_pos = (sw_mult(cr,3) + sw_mult(cg,5) + sw_mult(cb,7)+ sw_mult(ca,11)) & 63;
+            unsigned int packed = ((unsigned int)cr << 24) | ((unsigned int)cg << 16) | ((unsigned int)cb << 8)  | (unsigned int)ca;
+
+            if (running_array[index_pos] == packed) {
+                OUTPUT = index_pos;
+            } else {
+                running_array[index_pos] = packed;
+
+                dr = cr - r_prev;
+                dg = cg - g_prev;
+                db = cb - b_prev;
+
+                if (ca == a_prev &&
+                    dr >= -2 && dr <= 1 &&
+                    dg >= -2 && dg <= 1 &&
+                    db >= -2 && db <= 1) {
+                    unsigned char diff_byte = 0b01000000 |
+                        ((dr + 2) << 4) |
+                        ((dg + 2) << 2) |
+                        (db + 2);
+                    OUTPUT = diff_byte;
+                    r_prev = cr;
+                    g_prev = cg;
+                    b_prev = cb;
+                    continue;
+                }
+
+                if (ca == a_prev) {
+                    OUTPUT = 0xFE;
+                    OUTPUT = cr;
+                    OUTPUT = cg;
+                    OUTPUT = cb;
+                } else {
+                    OUTPUT = 0xFF;
+                    OUTPUT = cr;
+                    OUTPUT = cg;
+                    OUTPUT = cb;
+                    OUTPUT = ca;
+                }
+            }
+
+            r_prev = cr;
+            g_prev = cg;
+            b_prev = cb;
+            a_prev = ca;
+        }
+    }
+    if (rle > 0) {
+        OUTPUT = (0b11000000 | (rle - 1));
+        rle = -1;
+    }
+
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x00;
+    OUTPUT = 0x01;
 
     return 0;
 }
+
+
+
+
+
+// //#include <stdio.h>
+
+// #include "tcnt.h"
+// #include "sensor.h"
+
+// #define LED_BASEAxDDRESS 0x80000000
+
+// #define LED_REG0_ADDRESS (LED_BASEAxDDRESS + 0*4)
+
+// #define LED (*(volatile unsigned int *) LED_REG0_ADDRESS)
+
+// void irq_handler(unsigned int cause) {
+
+//         LED = 0x00FF00FF;
+
+// }
+
+// int main(void) {
+
+//     // unsigned char number;
+
+//     // number = SENSOR_get_width();
+
+//     // LED = number;
+
+//     // number = SENSOR_get_height();
+
+//     // LED = number;
+
+//     return 0;
+// }
 
     
