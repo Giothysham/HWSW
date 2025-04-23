df03d5d3d1be3aa9522ceb2266263931168c037b
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-04-23 10:23:58 +0200

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- hwswcd_template_ch4/hdl/PKG_hwswcd.vhd
+++ hwswcd_template_ch4/hdl/PKG_hwswcd.vhd
@@ -37,6 +37,7 @@ package PKG_hwswcd is
     constant C_DMEM_BASE_ADDRESS_MASK : STD_LOGIC_VECTOR(C_WIDTH-1 downto C_PERIPHERAL_MASK_LOWINDEX) := x"0000";
     constant C_LED_BASE_ADDRESS_MASK : STD_LOGIC_VECTOR(C_WIDTH-1 downto C_PERIPHERAL_MASK_LOWINDEX) := x"8000";
     constant C_TIMER_BASE_ADDRESS_MASK : STD_LOGIC_VECTOR(C_WIDTH-1 downto C_PERIPHERAL_MASK_LOWINDEX) := x"8100";
+    constant C_SENSOR_BASE_ADDRESS_MASK : std_logic_vector (C_WIDTH-1 downto C_PERIPHERAL_MASK_LOWINDEX) := x"8200";
 
     constant C_MRO_xF11_MVENDORID : STD_LOGIC_VECTOR(C_WIDTH-1 downto 0) := x"01234568";
     constant C_MRO_xF14_MHARTID : STD_LOGIC_VECTOR(C_WIDTH-1 downto 0) := x"CAFEBABE";
@@ -278,6 +279,20 @@ package PKG_hwswcd is
             data_out : out STD_LOGIC_VECTOR(31 downto 0)
         );
     end component two_k_bram_imem;
+    
+    component sensor is
+        generic(
+            G_PIXELS : natural := 3750
+        );
+        
+        port(
+        clock : in STD_LOGIC;
+        reset : in STD_LOGIC;
+        pixel_data_out_re : in STD_LOGIC;
+        pixel_data_out : out STD_LOGIC_VECTOR(31 downto 0);
+        first : out std_logic
+      );
+    end component sensor;
 
 end package;
 
