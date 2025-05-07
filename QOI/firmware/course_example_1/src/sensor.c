7fdb5059912e379c8376715d4d1cf9a11e8ff19a
giothysham <edmond.tsampanis@student.uhasselt.be>
2025-05-07 15:57:08 +0200

    hh
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

--- QOI/firmware/course_example_1/src/sensor.c
+++ QOI/firmware/course_example_1/src/sensor.c
@@ -3,8 +3,6 @@
 unsigned int SENSOR_fetch(void) {
     unsigned int pixeldata;
     pixeldata = SENSOR_PIXELDATA;
-    SENSOR_CR |= SENSOR_CR_RE; 
-    SENSOR_CR &= ~SENSOR_CR_RE; 
     return pixeldata;
 }
 
